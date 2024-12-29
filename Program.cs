aff5f276cd6e42adad8d3ed17a120c42913626f0
ogash3103 <faxogabek@gmail.com>
2024-12-29 23:25:13 +0500

    I have done three tasks

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+// task 1 start
+
+System.Console.Write("string so'z kiriting: ");
+string str = Console.ReadLine()!;
+System.Console.Write("Son kirgizing: ");
+int son = int.Parse(Console.ReadLine()!);
+
+string result = (son > str.Length) ? str.ToUpper() : str.ToLower();
+System.Console.WriteLine(result);
+// task 1 end
+
+
+
+// task 2 start
+
+int x = 15, 
+    y = 10;
+
+ if(x > y)
+ {
+    System.Console.WriteLine("x is greater than y");
+ } else if (x < y)
+ {
+    System.Console.WriteLine("x is less than y");
+ }  else if(x == y)
+ {
+    System.Console.WriteLine("x is equal to y");
+ }else {System.Console.WriteLine("a and y are not comparable");}
+
+// task 2 end
+
+// task 3 start
+
+System.Console.Write("Hafta kunini kirgizing O'zbek tilida: ");
+string input = Console.ReadLine()!;
+string weekDays = input switch 
+{
+   "Dushanba" => "Moday",
+   "Seshanba" => "Tueasday",
+   "Chorshanba" => "Wednesday",
+   "Payshanba" => "Thursday",
+   "Juma" => "Friday",
+   "Shanba" => "Saturday",
+   "Yakshanba" => "Sunday",
+   _ => "Xato ma'lumot, Qayta urunib ko'ring.."
+
+};
+System.Console.WriteLine(weekDays);
+
+// task 3 end
